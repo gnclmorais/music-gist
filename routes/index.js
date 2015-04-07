4fba24159580b32f3b9a25746b52adb3e25e6047
Gonçalo Morais <gnclmorais@gmail.com>
2015-04-07 08:01:45 -0400

    Whole bunch fo stuff.
    Specially for the homepage.

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-/* GET demo page. */
-router.get('/helloworld', function(req, res, next) {
-  res.render('helloworld', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Music Gist'
+  });
 });
 
 module.exports = router;
