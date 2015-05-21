9593e7fba17fac7cd40f602da1643cd35213276b
Gonçalo Morais <gnclmorais@gmail.com>
2015-05-21 11:57:12 +0100

    Small cleanup.

--- app.js
+++ app.js
@@ -8,31 +8,36 @@ var countries  = require('country-data').countries;
 var fs = require('fs');
 var nconf = require('nconf');
 
-//nconf.file({ file: 'config.json' });
+// Get environment variables
 nconf.env();
+var lastfmApikey = nconf.get('LASTFM_APIKEY');
+var lastfmSecret = nconf.get('LASTFM_SECRET');
 
-// get musicbrainz wrapper and initialise a client
+// Set a user agent for some services
+var userAgent = 'music-gist/v0.0.1';
+
+// Get musicbrainz wrapper and initialise a client
 var nb = require('nodebrainz');
 var mb = new nb({
-  userAgent:'musig-gist/0.0.1 ( https://github.com/gnclmorais/music-gist )'
+  userAgent: userAgent + '( https://github.com/gnclmorais/music-gist )'
 });
 
-// get Last.fm wrapper and initialise a client
+// Get Last.fm wrapper and initialise a client
 var LastFmNode = require('lastfm').LastFmNode;
 var fm = new LastFmNode({
-  api_key: nconf.get('LASTFM_APIKEY'),
-  secret: nconf.get('LASTFM_SECRET'),
-  useragent: 'music-gist/v0.0.1'
+  api_key: lastfmApikey,
+  secret: lastfmSecret,
+  useragent: userAgent
 });
 
-// get available routes
+// Get available routes
 var routes = require('./routes/index');
 var search = require('./routes/search');
 var artist = require('./routes/artist');
 
 var app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -44,7 +49,8 @@ app.use(cookieParser());
 app.use(require('stylus').middleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// make our MusicBrainz client accessible to our router
+// Make our clients accessible to our router,
+// as well as the list of countries
 app.use(function (req, res, next) {
     req.mb = mb;
     req.fm = fm;
@@ -52,22 +58,21 @@ app.use(function (req, res, next) {
     next();
 });
 
-// set routes
+// Set routes
 app.use('/', routes);
 app.use('/search', search);
 app.use('/artist', artist);
 
-// catch 404 and forward to error handler
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handlers
+// Error handlers
 
-// development error handler
-// will print stacktrace
+// Development error handler: will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
@@ -78,8 +83,7 @@ if (app.get('env') === 'development') {
   });
 }
 
-// production error handler
-// no stacktraces leaked to user
+// Production error handler: no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
@@ -87,6 +91,5 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
 
 module.exports = app;
